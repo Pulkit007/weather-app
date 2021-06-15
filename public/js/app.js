7e7ed04a741e63469d2479f84880d774ca41bd94
Pulkit007 <77740386+Pulkit007@users.noreply.github.com>
2021-06-16 02:59:55 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log("Successful");
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const p1 = document.querySelector("#msg-1");
+const p2 = document.querySelector("#msg-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  p1.innerHTML = "Loading...";
+  p2.innerHTML = "";
+  e.preventDefault();
+  const location = search.value;
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          return (p1.innerHTML = data.error);
+        }
+        p1.innerHTML = "Location : " + data.location;
+        p2.innerHTML = "Temperature : " + data.data;
+      });
+    }
+  );
+});
---
+++ src/app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+
+//defining paths
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
+
+//set up handlebars
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialPath);
+
+// static directory
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Pulkit",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "Weather App",
+    name: "Pulkit",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    msg: "Coming soon!",
+    name: "Pulkit",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide some address",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      } else {
+        forecast(latitude, longitude, (error, data) => {
+          if (error) {
+            return res.send({ error });
+          }
+          res.send({
+            location,
+            data: data,
+            address: req.query.address,
+          });
+        });
+      }
+    }
+  );
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("handle", {
+    error_msg: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("handle", {
+    error_msg: "My 404 page",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
